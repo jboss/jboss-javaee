63fdba1634671e4ffd25097b77bb6ed27cb775f7
Shelly McGowan <smcgowan@redhat.com>
2008-04-02 18:03:15 +0000

    sync with jpa-api v1_0_2_GA
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/javaee/trunk@71630 84be2c1e-ba19-0410-b317-a758671a6fc1

--- jboss-persistence-api/src/main/javax/persistence/TemporalType.java
+++ jboss-persistence-api/src/main/javax/persistence/TemporalType.java
@@ -1,9 +1,9 @@
-//$Id: TemporalType.java 11282 2007-03-14 22:05:59Z epbernard $
+//$Id: TemporalType.java 14298 2008-01-30 17:51:29Z epbernard $
 //EJB3 Specification Copyright 2004-2006 Sun Microsystems, Inc.
 package javax.persistence;
 
 /**
- * Type used to indicate a specific mapping of Date or Calendar.
+ * Type used to indicate a specific mapping of {@link java.util.Date} or {@link java.util.Calendar).
  */
 public enum TemporalType {
 	/**
