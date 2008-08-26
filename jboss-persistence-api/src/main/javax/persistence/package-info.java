33a4cf58cf4ad3e1f21895aea86a781ba5de320f
Shelly McGowan <smcgowan@redhat.com>
2008-08-26 15:06:49 +0000

    JBEE3 - remove persistence apis
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/javaee/trunk@77492 84be2c1e-ba19-0410-b317-a758671a6fc1

--- jboss-persistence-api/src/main/javax/persistence/package-info.java
+++
@@ -1,9 +0,0 @@
-//$Id:$
-//EJB3 Specification Copyright 2004-2006 Sun Microsystems, Inc.
-
-/**
- * The javax.persistence package contains the classes and interfaces that define the contracts
- * between a persistence provider and the managed classes and the clients of the Java Persistence API.
- */
-package javax.persistence;
-
